af014ebc2bfebd942367ba1d4bdf49f9d444e837
KhatrusovSN <113129616+KhatrusovSN@users.noreply.github.com>
2022-11-12 12:22:47 +0300

    Добавьте файлы проекта.

---
+++ 9.6FinalProject/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace _9._6FinalProject
+{
+    public class MyException : Exception
+    {
+        public MyException(string message) : base(message)
+        {
+            
+        }
+    }
+
+    class Sort
+    {
+        public event Action<string[]>? EventSort; // Создаем событие.
+        public void SortOptions(string[] names)
+        {
+            if ( names != null)
+            {
+                EventSort?.Invoke(names);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] secondNames = { "Смирнов", "Антонов", "Галкин", "Киркоров", "Петров" };
+
+            Sort sort = new();
+
+            Console.Write("Введите '1' для сортировки от А-Я, и '2' для сортировки от Я-А: ");
+            try
+            {
+                int value = Convert.ToInt32(Console.ReadLine());
+                
+                switch (value)
+                {
+                    case 1:
+                        sort.EventSort += SortOrderBy; // Подписка на событие.
+                        sort.SortOptions(secondNames);
+                        break;
+                    case 2:
+                        sort.EventSort += SortOrderByDescending; // Подписка на событие.
+                        sort.SortOptions(secondNames);
+                        break;
+                    default:
+                        value = 3;
+                        break;
+                }
+
+                if (value==3)
+                {
+                    throw new MyException("Сообщение исключения. ");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Неверный формат. " + ex.Message);
+            }
+            catch(MyException ex)
+            {
+                Console.WriteLine("Вызван мой метод ислючения. " + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void SortOrderBy(string[] secondNames) // Сортировка по возврастанию
+        {
+            IEnumerable<string> sortNames = from names in secondNames
+                                            orderby names[..1], names.Length
+                                            select names;
+            foreach (var name in sortNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        static void SortOrderByDescending(string[] secondNames) // Сортировка по убыванию
+        {
+            IEnumerable<string> sortNames = from names in secondNames
+                                              orderby names[..1] descending, names.Length
+                                              select names;
+            foreach (var name in sortNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exception = new Exception[5];
+            exception[0] = new FormatException("Format Exception");
+            exception[1] = new DivideByZeroException("Divide by zero");
+            exception[2] = new ArgumentException("Argument Exception");
+            exception[3] = new DirectoryNotFoundException("Directory Not Found");
+            exception[4] = new MyException("Exception text");
+            try
+            {
+                throw exception[4];
+
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+    public class MyException : Exception
+    {
+        public MyException(string message): base(message)
+        { 
+
+        }
+    }
+}
